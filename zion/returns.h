9ddfe9205e22c60b08f76d29c9bca930b36cd888
Paul <pfultz2@yahoo.com>
2012-10-01 18:42:02 -0400

    Updated returns

--- zion/returns.h
+++ zion/returns.h
@@ -11,12 +11,12 @@
 
 #include <zion/requires.h>
 
-#define ZION_RETURNS(...) -> decltype(__VA_ARGS__) { return (__VA_ARGS__); }
+// #define ZION_RETURNS(...) -> decltype(__VA_ARGS__) { return (__VA_ARGS__); }
 
-// #define ZION_RETURNS(...) \
-//         noexcept(noexcept(decltype(__VA_ARGS__)(std::move(__VA_ARGS__)))) \
-//         -> decltype(__VA_ARGS__) \
-//         { return (__VA_ARGS__); }
+#define ZION_RETURNS(...) \
+        noexcept(noexcept(decltype(__VA_ARGS__)(std::move(__VA_ARGS__)))) \
+        -> decltype(__VA_ARGS__) \
+        { return (__VA_ARGS__); } static_assert(true, "")
 
 #define ZION_ERROR_RETURN_REQUIRES_NEEDS_AN_EXPRESSION(...) decltype(__VA_ARGS__)>::type { return __VA_ARGS__; }
 #define ZION_RETURN_REQUIRES(...) -> typename zion::tpl::if_<ZION_REQUIRES_CLAUSE(__VA_ARGS__), ZION_ERROR_RETURN_REQUIRES_NEEDS_AN_EXPRESSION
