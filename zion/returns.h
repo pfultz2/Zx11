f5be7f92d052dbb215edac2c3f11b5f6399b9148
Paul <pfultz2@yahoo.com>
2012-10-23 08:27:30 -0400

    Add workaround for member access defect with trailing return types

--- zion/returns.h
+++ zion/returns.h
@@ -10,15 +10,34 @@
 #define ZION_GUARD_RETURNS_H
 
 #include <zion/requires.h>
+#include <chaos/preprocessor.h>
 
 // #define ZION_RETURNS(...) -> decltype(__VA_ARGS__) { return (__VA_ARGS__); }
 
-#define ZION_RETURNS(...) \
-        noexcept(noexcept(decltype(__VA_ARGS__)(std::move(__VA_ARGS__)))) \
-        -> decltype(__VA_ARGS__) \
-        { return (__VA_ARGS__); } static_assert(true, "")
+// This is a workaround for the defect report
+#define ZION_RETURNS_RETURN(...) return ZION_RETURNS_RETURN_X(CHAOS_PP_WALL(__VA_ARGS__))
+#define ZION_RETURNS_RETURN_X(...) __VA_ARGS__
 
-#define ZION_ERROR_RETURN_REQUIRES_NEEDS_AN_EXPRESSION(...) decltype(__VA_ARGS__)>::type { return __VA_ARGS__; }
+#define ZION_RETURNS_DECLTYPE(...) decltype(ZION_RETURNS_DECLTYPE_CONTEXT(__VA_ARGS__))
+
+#define ZION_RETURNS_DECLTYPE_CONTEXT(...) ZION_RETURNS_DECLTYPE_CONTEXT_X(CHAOS_PP_WALL(__VA_ARGS__))
+#define ZION_RETURNS_DECLTYPE_CONTEXT_X(...) __VA_ARGS__
+
+#define ZION_RETURNS_THIS() CHAOS_PP_IIF(CHAOS_PP_IS_NULLARY(ZION_RETURNS_DECLTYPE_CONTEXT(())))(this, zion_this())
+
+#define ZION_THIS CHAOS_PP_RAIL(ZION_RETURNS_THIS)()
+
+#define ZION_RETURNS_CLASS(...) static __VA_ARGS__* zion_this();
+
+//#define ZION_RETURNS(...) \
+//        noexcept(noexcept(decltype(__VA_ARGS__)(std::move(__VA_ARGS__)))) \
+//        -> decltype(__VA_ARGS__) \
+//        { return (__VA_ARGS__); } static_assert(true, "")
+
+
+#define ZION_RETURNS(...) -> ZION_RETURNS_DECLTYPE(__VA_ARGS__) { ZION_RETURNS_RETURN(__VA_ARGS__); } static_assert(true, "")
+
+#define ZION_ERROR_RETURN_REQUIRES_NEEDS_AN_EXPRESSION(...) ZION_RETURNS_DECLTYPE(__VA_ARGS__)>::type { ZION_RETURNS_RETURN(__VA_ARGS__); } static_assert(true, "")
 #define ZION_RETURN_REQUIRES(...) -> typename zion::tpl::if_<ZION_REQUIRES_CLAUSE(__VA_ARGS__), ZION_ERROR_RETURN_REQUIRES_NEEDS_AN_EXPRESSION
 
 #endif
