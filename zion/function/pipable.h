6ed70fbde2f5b5d3660ef63129063978afc0f89e
paul <paul@paul-TPOWER-X79>
2012-11-05 18:31:18 -0600

    Updated pipable to use bind instead of a tuple

--- zion/function/pipable.h
+++ zion/function/pipable.h
@@ -13,46 +13,43 @@
 #include <zion/is_callable.h>
 #include <zion/returns.h>
 #include <zion/invoke.h>
-#include <tuple>
+#include <functional>
 
 namespace zion {
 
+template<class T>
+ZION_FUNCTION_REQUIRES(std::is_rvalue_reference<T&&>)
+(T) auto_ref(T&& x)
+{
+    return std::forward<T>(x);
+}
+
+template<class T>
+ZION_FUNCTION_REQUIRES(not std::is_rvalue_reference<T&&>)
+(std::reference_wrapper<typename std::remove_reference<T>::type>) auto_ref(T&& x)
+{
+    return std::reference_wrapper<typename std::remove_reference<T>::type>(x);
+}
+
 //Pipable
 namespace details {
-template<class F, class Sequence>
+template<class F>
 struct pipe_closure
 {
     F f;
-    Sequence seq;
     
-    template<class S>
-    pipe_closure(S && seq) : seq(std::forward<S>(seq)) {};
-    
-    template<class S>
-    pipe_closure(F f, S && seq) : f(f), seq(std::forward<S>(seq)) {};
+    template<class X>
+    pipe_closure(X f) : f(f) {};
 
-//    template<class A>
-//    friend auto operator|(A && a, pipe_closure<F, Sequence> p) ZION_RETURNS
-//    (zion::invoke(p.f, std::tuple_cat
-//        (
-//            std::forward_as_tuple(std::forward<A>(a)), 
-//            std::forward<Sequence>(p.seq)
-//        ))
-//    );
 };
 
     template<class A, class F, class Sequence>
-    auto operator|(A && a, const pipe_closure<F, Sequence>& p) ZION_RETURNS
-    (zion::invoke(p.f, std::tuple_cat
-                  (
-                   std::forward_as_tuple(std::forward<A>(a)),
-                   std::forward<Sequence>(p.seq)
-                   ))
-     );
+    auto operator|(A && a, const pipe_closure<F>& p) ZION_RETURNS
+    (p.f(std::forward<A>(a)));
 
-template<class F, class Seq>
-auto make_pipe_closure(F f, Seq&& seq) ZION_RETURNS
-(pipe_closure<F, Seq&&>(f, std::forward<Seq>(seq)));
+template<class F>
+auto make_pipe_closure(F f) ZION_RETURNS
+(pipe_closure<F>(f));
 }
 
 
@@ -70,14 +67,10 @@ struct pipable_adaptor : function_adaptor_base<F>
     (ZION_THIS->get_function()(std::forward<T>(x)...));
 
     template<class... T>
-    auto operator()(T && ... x) ZION_RETURNS
+    auto operator()(T && ... x) ZION_RETURN_REQUIRES(not is_callable<F(T&&...)>)
     (
-        details::make_pipe_closure(ZION_THIS->get_function(), std::forward_as_tuple(std::forward<T>(x)...)) 
+        details::make_pipe_closure(ZION_THIS->get_function(), std::bind(std::placeholders::_1, auto_ref(std::forward<T>(x))...) ) 
     );
-
-//    template<class A>
-//    friend auto operator|(A && x, pipable_adaptor<F> p) ZION_RETURNS
-//    (p.get_function()(std::forward<A>(x)));
 };
 
     template<class A, class F>
